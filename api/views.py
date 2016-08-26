a579ce7fef878a770bb7f6e3b03daf56b7767456
shayan arman <shayanarman@shayans-MacBook-Pro.local>
2016-08-26 14:42:42 -0600

    Templating and CSS.

--- api/views.py
+++ api/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+class Post:
+    def __init__(self, title, published_date, num_likes):
+        self.title = title
+        self.published_date = published_date
+        self.num_likes = num_likes
+        self.text = 'I love everything about your store.'
+
+
+
 # Create your views here.
 def post_list(request):
-    return render(request, 'api/main.html', {})
+    posts = [Post('Street Style', 'August 29', 30), Post('Road Style', 'Sept', 1)]
+    return render(request, 'api/main.html', {'posts': posts})
